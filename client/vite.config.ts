e0849ba53c307ee60d8cb195713b3aa6e8bfbb01
abhas20 <darklegends2971@gmail.com>
2026-09-17 08:33:23 +0530

    feat(initialised): the structure of project

---
+++ client/vite.config.ts
@@ -0,0 +1,23 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
+
